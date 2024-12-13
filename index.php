4237224115ff68ff8bac653a37c0656d2bb35d49
玉木yumu <162607355+yumuwinter@users.noreply.github.com>
2024-12-13 13:17:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- TITLE-->
+
+	<title>DepED ECD Checklist - Home</title>
+
+
+	<!-- STYLE -->
+
+	<link rel="stylesheet" href="style/main.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/740de17fe5.js" crossorigin="anonymous"></script>
+
+</head>
+<body style ="">
+	<!-- HEADER -->
+	<header>
+		<img class="ecd_logo" src="logos/ecd_logo.png" alt="ECD Logo">
+	</header>
+
+<section>
+
+	<div class="index">
+
+		<h1>DepED ECD Checklist</h1>
+
+		<div class="index_container">
+			<h3>School Admin Log-in <i class="fa-solid fa-user"></i></h3>
+			<a href="principal/login.php"><button><strong>Log-in</strong><i class="fa-solid fa-arrow-right"></i></button></a>
+		</div>
+
+		<div class="index_container">
+			<h3>Teacher Log-in <i class="fa-solid fa-chalkboard-user"></i></h3>
+			<a href="teacher/login.php"><button><strong>Log-in</strong><i class="fa-solid fa-arrow-right"></i></button></a>
+		</div>
+
+	</div>
+
+</section>
+	
+
+</body>
+</html>
